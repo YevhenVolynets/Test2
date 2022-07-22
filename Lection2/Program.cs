11b311e3124210fec5358c82e907f01242c79056
Yevhen Volynets <yvolynets@exoft.net>
2022-07-22 13:50:48 +0300

    Added long feature 2

--- Lection2/Program.cs
+++ Lection2/Program.cs
@@ -9,6 +9,8 @@ namespace Lection2
             Console.WriteLine("Hello World from branch1 and branch2!");
             var f2 = new object(); //feature2
             var f1 = new object(); //feature1
+
+            Console.WriteLine("Very long feature 2");
         }
     }
 }
